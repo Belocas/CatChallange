1005bbfc9796cb2e4f8053c031baade82f67ee64
isabelcouto <isabelsantoscouto@gmail.com>
2025-07-04 21:42:45 +0100

    First commit

---
+++ Cats/Cats/CatsApp.swift
@@ -0,0 +1,20 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by Isabel Couto on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
