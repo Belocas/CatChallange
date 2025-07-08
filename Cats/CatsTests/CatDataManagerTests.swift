7a00b38b00b1a16f5b4a46de631e132aa5b6a849
isabelcouto <isabelsantoscouto@gmail.com>
2025-07-08 22:11:43 +0100

    Add unit test and app icons

--- Cats/CatsTests/CatDataManagerTests.swift
+++ Cats/CatsTests/CatDataManagerTests.swift
@@ -12,7 +12,7 @@ import CoreData
 final class CatDataManagerTests: XCTestCase {
     
     var inMemoryContext: NSManagedObjectContext!
-    var dataManager: CatDataManagerTests!
+    var dataManager: CatDataManager!
     
     
     override func setUp() {
@@ -33,27 +33,33 @@ final class CatDataManagerTests: XCTestCase {
         dataManager = CatDataManager(context: inMemoryContext)
     }
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    override  func tearDown() {
+        inMemoryContext = nil
+        dataManager = nil
+        super.tearDown()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    
+    func testSaveAndCountCats(){
+        let cats = [
+            Cat(id: "1", name: "Milo", temperament: "UK", origin: "Calm", description: "Friendly cat", imageId: ""),
+            Cat(id: "2", name: "Luna", temperament: "PT", origin: "Playful", description: "Very active", imageId: ""),
+        ]
+        dataManager.save(cats: cats)
+        let count = dataManager.countCats(in: inMemoryContext)
+        XCTAssertEqual(count, 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testPreventDuplicateCats(){
+        let cats = [
+            Cat(id: "1", name: "Milo",temperament: "Calm" ,  origin: "UK", description: "Friendly cat",imageId: "")
+        ]
+        dataManager.save(cats: cats)
+               
+        let loadedCats = dataManager.loadCatsFromCoreData(context: inMemoryContext)
+        XCTAssertEqual(loadedCats.count, 1)
+        XCTAssertEqual(loadedCats.first?.name, "Milo")
     }
 
 }
